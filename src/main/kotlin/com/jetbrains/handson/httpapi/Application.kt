f0170db6de06754496931f1a8f5f0c3c41c76270
Yunhao Liu <yunhao981@hotmail.com>
2022-02-17 00:39:34 +0800

    write CustomerRoutes follwing the doc

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -6,5 +6,8 @@ import io.ktor.application.Application
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomerRoutes()
 }
---
+++ src/main/kotlin/com/jetbrains/handson/routes/CustomerRoutes.kt
@@ -0,0 +1,50 @@
+import io.ktor.routing.*
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+            if (customerStorage.isNotEmpty()) {
+                call.respond(customerStorage)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.NotFound)
+            }
+        }
+        get("{id}") {
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = HttpsStatusCode.NotFound
+                )
+            call.respond(customer)
+        }
+        post {
+            val custmer = call.receive<Customer>()
+            customerStorage.add(custmer)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
+
+        }
+        delete("{id}") {
+            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
+            if (customerStorage.removeIf { it.id == id}) {
+                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("Not Found", status =HttpStatusCode.NotFound)
+            }
+        }
+    }
+}
+
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
+    }
+}
