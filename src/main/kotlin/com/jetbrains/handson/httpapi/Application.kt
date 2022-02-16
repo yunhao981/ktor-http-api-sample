097d020a42ad0a6145b6d54260151db33d6c8dd3
Yunhao Liu <yunhao981@hotmail.com>
2022-02-17 01:20:16 +0800

    fix typo and cahnged folder paths

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,7 +1,9 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
-
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
+import registerCustomerRoutes
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -22,7 +22,7 @@ fun Route.customerRouting() {
             val customer =
                 customerStorage.find { it.id == id } ?: return@get call.respondText(
                     "No customer with id $id",
-                    status = HttpsStatusCode.NotFound
+                    status = HttpStatusCode.NotFound
                 )
             call.respond(customer)
         }
